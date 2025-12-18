8e018a10cdd43bc88a46392f45ad8b6d827d5074
Jiho Bok <bok.jiho@gmail.com>
2025-12-18 17:03:18 +0000

    refactor: remove unused code

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -30,7 +30,6 @@ export class Preloader extends Scene {
     //  Load the assets for the game - Replace with your own assets
     this.load.setPath("assets");
 
-    this.load.image("logo", "logo.png");
     this.load.image("star", "star.png");
     this.load.image("cash", "/items/cash.svg");
   }
