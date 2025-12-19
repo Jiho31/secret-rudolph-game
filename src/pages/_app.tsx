77bb9fbbe81e6127d74bac5ecbe8d5e0bf8b0f2a
Jiho Bok <bok.jiho@gmail.com>
2025-12-19 19:37:45 +0000

    feat: update app metadata and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Rudolph's Present Game</title>
+        <meta
+          name="description"
+          content="Play Rudolph's Presents Game to find out what you're friends & families want for Christmas"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,13 @@
-import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import dynamic from "next/dynamic";
 import MainPage from "./main";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// const AppWithoutSSR = dynamic(() => import("@/App"), { ssr: false });
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Rudolph's Present Game</title>
-        <meta
-          name="description"
-          content="Play Rudolph's Presents Game to find out what you're friends & families want for Christmas"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        {/* {router.query?.gameId ? <MainPage /> : <AppWithoutSSR />} */}
-        {/* <AppWithoutSSR /> */}
         <MainPage />
       </main>
     </>
